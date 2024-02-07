3e20203992bce6149c25406f95be1680aad609f8
uromanc <ourcssdev@gmail.com>
2024-02-07 16:45:35 -0600

    Header component with navbar done

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      'urcp': {
+        'wfc': '#FCFCFC',
+        'carolinab': '#8EB5C4',
+        'moonstone': '#669AA6',
+        'cerulean': '#1F6E8C',
+        'midnightg': '#104B62',
+        'eerienb': '#161818',
+        'gunmetal': '#13323D',
+        'charcoal': '#29464E',
+        'dsgray': '#3E595F',
+        'pgray': '#50686E',
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
